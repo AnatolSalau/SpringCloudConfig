28ffd7f0bd1a04ca7e6709231d27ff4147ecca00
AnatolSalau <salovanatoly5@mail.ru>
2024-01-08 22:29:48 +0300

    Spring 8888

--- spring_web_client/src/main/java/com/example/spring_web_client/SpringWebClientApplication.java
+++ spring_web_client/src/main/java/com/example/spring_web_client/SpringWebClientApplication.java
@@ -1,5 +1,6 @@
 package com.example.spring_web_client;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,11 @@ public class SpringWebClientApplication {
 
       public static void main(String[] args) {
             SpringApplication.run(SpringWebClientApplication.class, args);
+      }
+
+      @PostConstruct
+      private void init() {
+            System.out.println("Init method");
       }
 
 }
